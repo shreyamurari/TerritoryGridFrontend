e85f9618470d77e5f2cfd5d3cd1047799c4de74d
shreyamurari <shreya.murari123@email.com>
2026-06-05 19:08:29 +0530

    features modified gitignore

---
+++ shared/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.TOTAL_CELLS = exports.GRID_SIZE = void 0;
+exports.GRID_SIZE = 50;
+exports.TOTAL_CELLS = exports.GRID_SIZE * exports.GRID_SIZE;
